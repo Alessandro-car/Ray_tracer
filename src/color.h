b9719381941b8e610ad1cd294ec53cd373f5bfa1
ale_carella <useradd@DESKTOP-U4GSC2L>
2024-12-30 21:26:55 +0100

    Generating a .ppm image using a camera

--- src/color.h
+++ src/color.h
@@ -2,11 +2,20 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include <iostream>
 
-#include <stdio.h>
+using color = vec3;
 
-typedef vec3 color;
+void write_color(std::ostream& out, const color& pixel_color) {
+	double r = pixel_color.x();
+	double g = pixel_color.y();
+	double b = pixel_color.z();
 
-void write_color(FILE* out, const color pixel_color);
+	int rbyte = int(255.999 * r);
+	int gbyte = int(255.999 * g);
+	int bbyte = int(255.999 * b);
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
 
 #endif
--- src/vec3.h
+++ src/vec3.h
@@ -1,65 +1,101 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-typedef struct {
-	double e[3];
-} vec3;
+#include <cmath>
+#include <iostream>
 
-typedef vec3 point3;
+class vec3 {
+	public:
+		double e[3];
+		vec3() : e{0, 0, 0} {}
+		vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-vec3 vec3_create(double e0, double e1, double e2);
+		double x() const{
+			return e[0];
+		}
 
-vec3 vec3_zero(void);
+		double y() const{
+			return e[1];
+		}
 
-double x(const vec3 *v);
+		double z() const {
+			return e[2];
+		}
 
-double y(const vec3 *v);
+		vec3& operator+=(const vec3& v) {
+			e[0] += v.e[0];
+			e[1] += v.e[1];
+			e[2] += v.e[2];
 
-double z(const vec3 *v);
+			return *this;
+		}
 
-double vec3_get(const vec3 *v, int i);
+		vec3& operator*=(double t) {
+			e[0] *= t;
+			e[1] *= t;
+			e[2] *= t;
 
-void vec3_set(vec3* v, int i, double value);
+			return *this;
+		}
 
-vec3 vec3_negate(const vec3 *v);
+		vec3& operator/=(double t) {
+			e[0] /= t;
+			e[1] /= t;
+			e[2] /= t;
 
-vec3 vec3_add(const vec3* u, const vec3 *v);
+			return *this;
+		}
 
-void vec3_add_assign(vec3 *u, const vec3 *v);
+		double length() const {
+			return std::sqrt(length_squared());
+		}
 
-vec3 vec3_subtract(const vec3 *u, const vec3 *v);
+		double length_squared() const {
+			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+		}
+};
 
-vec3 vec3_multiply(const vec3 *u, const vec3 *v);
+using point3 = vec3;
 
-vec3 vec3_multiply_scalar(const vec3 *v, double t);
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
 
-void vec3_multiply_assign(vec3 *v, double t);
+inline vec3 operator+(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
 
-vec3 vec3_divide(const vec3 *v, double t);
+inline vec3 operator-(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
 
-void vec3_divide_assign(vec3 *v, double t);
+inline vec3 operator*(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
 
-double vec3_length_squared(const vec3* v);
+inline vec3 operator*(double t, const vec3& v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
 
-double vec3_length(const vec3 *v);
+inline vec3 operator/(const vec3& v, double t) {
+	return (1 / t) * v;
+}
 
-double vec3_dot(const vec3* u, const vec3* v);
+inline double dot(const vec3& v, const vec3& u) {
+	return u.e[0] * v.e[0]
+			 + u.e[1] * v.e[1]
+			 + u.e[2] * v.e[2];
+}
 
-vec3 vec3_cross(const vec3* u, const vec3 *v);
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+              u.e[2] * v.e[0] - u.e[0] * v.e[2],
+              u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
 
-vec3 vec3_unit_vector(const vec3 *v);
-
-void vec3_print(const vec3* v);
-
-#define VEC3_NEG(v) vec3_negate(&(v))
-#define VEC3_ADD(u, v) vec3_add(&(u), &(v))
-#define VEC3_ADD_ASSIGN(u, v) vec3_add_assign(&(u), &(v))
-#define VEC3_SUB(u, v) vec3_subtract(&(u), &(v))
-#define VEC3_MUL(u, v) _Generic((v), \
-												double: vec3_multiply_scalar(&(u), v)) \
-												vec3: vec3_multiply(&(u), &(v)))
-#define VEC3_DIV(u, t) vec3_divide(&(u), (t))
-#define VEC3_MUL_ASSIGN(u, t) vec3_multiply_assign(&(u), (t))
-#define VEC3_DIV_ASSIGN(u, t) vec3_divide_assign(&(u), (t))
+inline vec3 unit_vector(const vec3& v) {
+	return v / v.length();
+}
 
 #endif
+
